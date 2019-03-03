f1c8e25d2ad1b8d4aa768af7803878665dcce7e1
elvineshan <30416440+elvineshan@users.noreply.github.com>
2019-03-03 19:17:54 +0000

    Created start button to initialise

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
-//Pull the photos data from the API
-fetch('https://jsonplaceholder.typicode.com/photos') //use API URL with '/photos' endpoint 
-  .then(response => response.json())
-  .then(json => { 
-  	//generate the correct <img> tags to load each photo from the JSON photos array 
-	//first create an empty variable
-  	var images = '';
-    for( var i=0; i<12; ++i ) {
-    	//use the thumbnail URL pulled from JSON to generate the <img> tag, for each instance in the array
-    	//store the result in the images variable
-    	images += '<img src="' + json[i].thumbnailUrl + '" />';
-    }
-    //populate the photoContainer element with the resultant images variable, that is, the photo returned from JSON
-    document.getElementById("photoContainer").innerHTML = images;
-  })
+document.getElementById("startbutton").addEventListener("click", showPhotos);
+
+function showPhotos() {
+	//Pull the photos data from the API
+	fetch('https://jsonplaceholder.typicode.com/photos')
+	.then(response => response.json())
+	.then(json => {
+		//generate the correct <img> tags to load each photo from the JSON photos array
+	 	//first create an empty variable
+		var images = '';
+		for( var i=0; i<12; ++i ) {
+			//use the thumbnail URL pulled from JSON to generate the <img> tag, for each instance in the array
+	  		//store the result in the images variable
+			images += '<img src="' + json[i].thumbnailUrl + '" />';
+		}
+	  	//populate the photoContainer element with the resultant images variable, that is, the photo returned from JSON		
+		document.getElementById("photoContainer").innerHTML = images;
+	})
+}
